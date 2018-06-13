ac4fa9b66a794a7ba80fabf1ef9d0da971462dcc
StevenGrl <steven.grillon.45@gmail.com>
2018-06-13 18:51:45 +0200

    room entity

---
+++ src/AppBundle/Controller/RoomController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Room;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Room controller.
+ *
+ * @Route("room")
+ */
+class RoomController extends Controller
+{
+    /**
+     * Lists all room entities.
+     *
+     * @Route("/", name="room_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $rooms = $em->getRepository('AppBundle:Room')->findAll();
+
+        return $this->render('room/index.html.twig', array(
+            'rooms' => $rooms,
+        ));
+    }
+
+    /**
+     * Creates a new room entity.
+     *
+     * @Route("/new", name="room_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $room = new Room();
+        $form = $this->createForm('AppBundle\Form\RoomType', $room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($room);
+            $em->flush();
+
+            return $this->redirectToRoute('room_show', array('id' => $room->getId()));
+        }
+
+        return $this->render('room/new.html.twig', array(
+            'room' => $room,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a room entity.
+     *
+     * @Route("/{id}", name="room_show")
+     * @Method("GET")
+     */
+    public function showAction(Room $room)
+    {
+        $deleteForm = $this->createDeleteForm($room);
+
+        return $this->render('room/show.html.twig', array(
+            'room' => $room,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing room entity.
+     *
+     * @Route("/{id}/edit", name="room_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Room $room)
+    {
+        $deleteForm = $this->createDeleteForm($room);
+        $editForm = $this->createForm('AppBundle\Form\RoomType', $room);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('room_edit', array('id' => $room->getId()));
+        }
+
+        return $this->render('room/edit.html.twig', array(
+            'room' => $room,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a room entity.
+     *
+     * @Route("/{id}", name="room_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Room $room)
+    {
+        $form = $this->createDeleteForm($room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($room);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('room_index');
+    }
+
+    /**
+     * Creates a form to delete a room entity.
+     *
+     * @param Room $room The room entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Room $room)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('room_delete', array('id' => $room->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Room.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Room
+ *
+ * @ORM\Table(name="room")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RoomRepository")
+ */
+class Room
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="theme", type="string", length=255)
+     */
+    private $theme;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Room
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set theme
+     *
+     * @param string $theme
+     *
+     * @return Room
+     */
+    public function setTheme($theme)
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    /**
+     * Get theme
+     *
+     * @return string
+     */
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+}
