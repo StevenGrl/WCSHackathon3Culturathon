f4b3af5a05d18861cad5ccffad81858dfeaa9891
StevenGrl <steven.grillon.45@gmail.com>
2018-06-13 22:52:00 +0200

    add setter treasure

--- src/AppBundle/DataFixtures/AppFixtures.php
+++ src/AppBundle/DataFixtures/AppFixtures.php
@@ -38,6 +38,7 @@ class AppFixtures extends Fixture
                 $tile->setType($type);
                 $tile->setCoordX($x);
                 $tile->setCoordY($y);
+                $tile->setHasTreasure(false);
                 $manager->persist($tile);
             }
         }
