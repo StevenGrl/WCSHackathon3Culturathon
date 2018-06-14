b8fa0b9a0d47faf3c7c10dd6a84a9cea9cb45de1
Raphael Mouton <raphaelmouton@live.fr>
2018-06-15 00:21:46 +0200

    add fav

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $em = $this->getDoctrine()->getManager();
+        $favorite2 = $em->getRepository('AppBundle:Favorite')->findAll();
+
+        return $this->render('default/index.html.twig', array(
+            'favorites' => $favorite2,
+        ));
     }
+
 }
