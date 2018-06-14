e0ab810997cb7b407192deaed8154db1c5d7bac1
StevenGrl <steven.grillon.45@gmail.com>
2018-06-14 12:40:21 +0200

    ajax working

--- assets/js/moveFinder.js
+++ assets/js/moveFinder.js
@@ -2,7 +2,9 @@ $(document).on('click', '.move', function (e) {
     e.preventDefault();
     let move = $(this).attr('data-move');
     $.get('/finder/direction/' + move).done(function (result) {
-       const mapResult = $(result).find('.map');
-       $('.map').replaceWith(mapResult);
+        // const alertResult = $(result).find('#message');
+        // $('#message').replaceWith(alertResult);
+        const mapResult = $(result).find('#map');
+        $('#map').replaceWith(mapResult);
     });
 });
