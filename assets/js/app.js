40b53f1c35ca1ec895af787ae6835c1b1c5bbdec
SteadyB <16727826+SteadyB@users.noreply.github.com>
2018-06-14 02:14:21 +0200

    navbar jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
 import $ from "jquery"
 
 import 'bootstrap/dist/js/bootstrap'
+
+$(window).resize(function() {
+    if ($(this).width() >= 1280) {
+        $("#navbar").removeClass("fixed-bottom").addClass("fixed-top");
+    }
+    else if ($(this).width() < 1280 && $(this).width()>= 480) {
+        $("#navbar").removeClass("fixed-top").addClass("fixed-bottom");
+    }
+});
