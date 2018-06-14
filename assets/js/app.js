b8f380f22db24af17c045adf2a6300f4586ea13d
SteadyB <16727826+SteadyB@users.noreply.github.com>
2018-06-14 02:38:37 +0200

    jquery dropdown/up

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,28 @@ import $ from "jquery"
 
 import 'bootstrap/dist/js/bootstrap'
 
+$(window).ready(function() {
+    if ($(this).width() >= 1280) {
+        $("#navbar").addClass("fixed-top");
+        $("#drop_one").addClass("dropdown");
+        $("#drop_two").addClass("dropdown");
+    }
+    else if ($(this).width() < 1280 && $(this).width()>= 0) {
+        $("#navbar").addClass("fixed-bottom");
+        $("#drop_one").addClass("dropup");
+        $("#drop_two").addClass("dropup");
+    }
+});
+
 $(window).resize(function() {
     if ($(this).width() >= 1280) {
         $("#navbar").removeClass("fixed-bottom").addClass("fixed-top");
+        $("#drop_one").addClass("dropdown").removeClass("dropup");
+        $("#drop_two").addClass("dropdown").removeClass("dropup");
     }
-    else if ($(this).width() < 1280 && $(this).width()>= 480) {
+    else if ($(this).width() < 1280 && $(this).width()>= 0) {
         $("#navbar").removeClass("fixed-top").addClass("fixed-bottom");
+        $("#drop_one").addClass("dropup").removeClass("dropdown");
+        $("#drop_two").addClass("dropup").removeClass("dropdown");
     }
 });
