020921eae9c578e145fcdd8581ccaac2d6a221ab
Raphael Mouton <raphaelmouton@live.fr>
2018-06-14 21:55:51 +0200

    version 22h

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,6 @@ $(window).ready(function() {
         $("#navbar").addClass("fixed-bottom");
         $("#drop_one").addClass("dropup");
         $("#drop_two").addClass("dropup");
-
     }
 });
 
@@ -33,6 +32,7 @@ $(window).resize(function() {
         $("#drop_one").addClass("dropup").removeClass("dropdown");
         $("#drop_two").addClass("dropup").removeClass("dropdown");
 
+
     }
 });
 
@@ -43,6 +43,7 @@ $('#collapseHidden').click(function(){
     $('#hidetext').hide();
     $('#pills-tab').hide();
     $('#pills-tabContent').hide();
+    $('#fav').hide();
 
 });
 $('#collapseShow').click(function(){
